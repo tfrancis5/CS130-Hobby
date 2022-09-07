1bfce72a11b76b6ccb13880c1b50aea172cae19c
tfrancis5 <112411889+tfrancis5@users.noreply.github.com>
2022-09-07 11:57:44 -0400

    Add files via upload

---
+++ TakeHome1.java
@@ -0,0 +1,9 @@
+public class TakeHome1
+{
+public static void main (String[]args)
+{
+System.out.println("My First Name is: "+args[0]);
+System.out.println("My Last Name is: "+args[1]);
+System.out.println("My Hobbies are: "+args[2]);
+System.out.println("My Major is: "+args[3]);
+}}
